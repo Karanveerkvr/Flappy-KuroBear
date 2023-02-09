975169d9be892b06015e1e021e217e1c02138d02
Karanveerkvr <kvrstk@gmail.com>
2023-02-09 19:04:20 +0530

    first commit

---
+++ Assets/PipeSpawnScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawnScript : MonoBehaviour
+{
+
+    public GameObject pipe;
+    public float spawnRate = 1;
+    private float timer = 0;
+    public float heightOffset = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < spawnRate)
+        {
+            timer = timer + Time.deltaTime;
+
+        }
+
+        else
+        {
+            spawnPipe();
+            timer = 0;
+        }
+
+        void spawnPipe()
+        {
+            float lowestpoint = transform.position.y - heightOffset;
+            float highestPoint = transform.position.x + heightOffset;
+
+            Instantiate(pipe, new Vector3(transform.position.x, Random.Range(lowestpoint, highestPoint), 0), transform.rotation);
+        }
+
+    }
+}
+
+   
